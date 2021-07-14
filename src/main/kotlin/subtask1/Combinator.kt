583abbdb2c5df0672051f47fa3ab201e35361305
LexIt <33904303+LexIt@users.noreply.github.com>
2021-07-14 22:37:29 +0300

    Combinator done

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -1,9 +1,37 @@
 package subtask1
 
+import java.math.BigInteger
+
 class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+
+        var res_K: Int? = null
+
+        val src_C = array[0]
+        val n = array[1]
+
+        for(i in 1..n){
+            var C = fact(n)/fact(i)/fact(n-i)
+            if(src_C <= C) {
+                if(res_K == null)
+                {
+                    res_K = i
+                }
+                else{
+                    if (res_K > i){
+                        res_K=i
+                    }
+                }
+            }
+        }
+
+        return res_K
+    }
+
+    fun fact(f: Int): Long {
+        if(f == 1 || f == 0) return 1
+        return f*fact(f-1)
     }
 }
